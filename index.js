61cdd7b27b91878bb85bb54fdcaaa86e5da6f57c
Mikadolebg <34174177+Mikadolebg@users.noreply.github.com>
2018-05-28 13:26:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ const prefix = '--'
 //Lancement et etat du bot
 bot.on('ready', () => {
    console.log('INFORMATIONS DU BOT:');
-   console.log('Invite of bot all perms: https://discordapp.com/oauth2/authorize?client_id=${bot.id}&scope=bot&permissions=2146958591');
-   console.log('username: ${bot.user.tag}');
-   console.log('ID: ${bot.id}');
-   console.log('Date of creation: ${bot.createdTimestamp}');
-   console.log('INFORMATIONS OF SERVER BOT:');
-   console.log('How many server: ${bot.guilds.size}');
+ //  console.log('Invite of bot all perms: https://discordapp.com/oauth2/authorize?client_id=${bot.id}&scope=bot&permissions=2146958591');
+ //  console.log('username: ${bot.user.tag}');
+  // console.log('ID: ${bot.id}');
+  // console.log('Date of creation: ${bot.createdTimestamp}');
+ //  console.log('INFORMATIONS OF SERVER BOT:');
+  // console.log('How many server: ${bot.guilds.size}');
   // console.log('Name: ${bot.guilds.name}');
   // console.log('Name: ${bot.guilds.id}');
   // console.log('How many member: ${guilds.memberCount}');
