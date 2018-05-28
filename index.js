9fee65b9ca332eb621bce68ba7052166c8b71e88
Mikadolebg <34174177+Mikadolebg@users.noreply.github.com>
2018-05-28 15:23:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,28 @@ const prefix = '--'
 
 //Lancement et etat du bot
 bot.on('ready', () => {
-   //console.log(`(1) Logged in as ${bot.user.tag}!`);
+   console.log(`        \          SORRY            /`);
+   console.log(`      \                         /`);
+   console.log(`       \    This page does     /`);
+   console.log(`        ]   not exist yet.    [    ,'|`);
+   console.log(`        ]                     [   /  |`);
+   console.log(`        ]___               ___[ ,'   |`);
+   console.log(`        ]  ]\             /[  [ |:   |`);
+   console.log(`        ]  ] \           / [  [ |:   |`);
+   console.log(`        ]  ]  ]         [  [  [ |:   |`);
+   console.log(`        ]  ]  ]__     __[  [  [ |:   |`);
+   console.log(`        ]  ]  ] ]\ _ /[ [  [  [ |:   |`);
+   console.log(`        ]  ]  ] ] (#) [ [  [  [ :===='`);
+   console.log(`        ]  ]  ]_].nHn.[_[  [  [`);
+   console.log(`        ]  ]  ]  HHHHH. [  [  [`);
+   console.log(`        ]  ] /   'UHH("  \ [  [`);
+   console.log(`        ]__]/     HHH  "  \[__[`);
+   console.log(`        ]         NNN         [`);
+   console.log(`        ]         N/"         [`);
+   console.log(`        ]         N H         [`);
+   console.log(`       /          N            \ `);
+   console.log(`      /           q'            \ `);
+   console.log(`     /                           \ `);
 
    console.log(`INFORMATIONS DU BOT:`);
    console.log(`Invite of bot all perms: https://discordapp.com/oauth2/authorize?client_id=${bot.user.id}&scope=bot&permissions=2146958591`);
