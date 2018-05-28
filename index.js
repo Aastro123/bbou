cb03089e7e6d0c764a58ef780133bbfc652d99f3
Mikadolebg <34174177+Mikadolebg@users.noreply.github.com>
2018-05-28 14:29:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,16 +13,16 @@ const prefix = '--'
 bot.on('ready', () => {
    //console.log(`(1) Logged in as ${bot.user.tag}!`);
 
-   console.log('INFORMATIONS DU BOT:');
-   console.log('Invite of bot all perms: https://discordapp.com/oauth2/authorize?client_id=${bot.id}&scope=bot&permissions=2146958591');
-   console.log('username: ${bot.user.tag}');
-   console.log('ID: ${bot.id}');
-   console.log('Date of creation: ${bot.createdTimestamp}');
-   console.log('INFORMATIONS OF SERVER BOT:');
-   console.log('How many server: ${bot.guilds.size}');
-   console.log('Name: ${bot.guilds.name}');
-   console.log('Name: ${bot.guilds.id}');
-   console.log('How many member: ${guilds.memberCount}');
+   console.log(`INFORMATIONS DU BOT:`);
+   console.log(`Invite of bot all perms: https://discordapp.com/oauth2/authorize?client_id=${bot.id}&scope=bot&permissions=2146958591`);
+   console.log(`username: ${bot.user.tag}`);
+   console.log(`ID: ${bot.id}`);
+   console.log(`Date of creation: ${bot.createdTimestamp}`);
+   console.log(`INFORMATIONS OF SERVER BOT:`);
+   console.log(`How many server: ${bot.guilds.size}`);
+   console.log(`Name: ${bot.guilds.name}`);
+   console.log(`Name: ${bot.guilds.id}`);
+   console.log(`How many member: ${guilds.memberCount}`);
  });
 
 bot.on('message', msg => {
