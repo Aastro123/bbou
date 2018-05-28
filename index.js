86d482e909ed4f17dfc013b163c0b5a684b3a3fe
Mikadolebg <34174177+Mikadolebg@users.noreply.github.com>
2018-05-28 21:42:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -27,8 +27,9 @@ bot.on('ready', () => {
    bot.guilds.forEach(guild => { 
      var invite = bot.guilds.find("id", guild.id).channels.find("id", guild.channels.random().id);
      invite.createInvite({maxAge: 0}).then(invite => console.log(`Connecté sur : ${guild.name} || ${invite} || ${guild.id)`));
-  })
-   
+     })
+  }
+
  });
 
 
