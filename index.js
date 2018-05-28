7ab46bb848700bb6135d4b3793ca0e9e0f359b1c
Mikadolebg <34174177+Mikadolebg@users.noreply.github.com>
2018-05-28 14:54:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,15 +14,15 @@ bot.on('ready', () => {
    //console.log(`(1) Logged in as ${bot.user.tag}!`);
 
    console.log(`INFORMATIONS DU BOT:`);
-   console.log(`Invite of bot all perms: https://discordapp.com/oauth2/authorize?client_id=${bot.id}&scope=bot&permissions=2146958591`);
+   console.log(`Invite of bot all perms: https://discordapp.com/oauth2/authorize?client_id=${bot.user.id}&scope=bot&permissions=2146958591`);
    console.log(`username: ${bot.user.tag}`);
-   console.log(`ID: ${bot.id}`);
-   console.log(`Date of creation: ${bot.createdTimestamp}`);
+   console.log(`ID: ${bot.user.id}`);
+   //console.log(`Date of creation: ${bot.createdTimestamp}`);
    console.log(`INFORMATIONS OF SERVER BOT:`);
    console.log(`How many server: ${bot.guilds.size}`);
-   console.log(`Name: ${bot.guilds.name}`);
-   console.log(`Name: ${bot.guilds.id}`);
-   console.log(`How many member: ${guilds.memberCount}`);
+  // console.log(`Name: ${bot.guilds.name}`);
+  // console.log(`ID: ${bot.guilds.id}`);
+ //  console.log(`How many member: ${guilds.memberCount}`);
  });
 
 bot.on('message', msg => {
