c5aafb65dbd19eb435036b146b7f17bda8c25b00
Mikadolebg <34174177+Mikadolebg@users.noreply.github.com>
2018-05-28 13:56:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ bot.on('ready', () => {
   // console.log('Name: ${bot.guilds.name}');
   // console.log('Name: ${bot.guilds.id}');
   // console.log('How many member: ${guilds.memberCount}');
-  //bot.user.setActivity('discord.js update', {type: "PLAYING"});
+  bot.user.setActivity('discord.js update', {type: "PLAYING"});
   });
 
 bot.on('message', msg => {
