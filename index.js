65e71252d30245423b1adb68ee4e6580008ff0c9
Mikadolebg <34174177+Mikadolebg@users.noreply.github.com>
2018-05-28 19:38:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ bot.on('ready', () => {
    console.log(`Invite of bot all perms: https://discordapp.com/oauth2/authorize?client_id=${bot.user.id}&scope=bot&permissions=2146958591`);
    console.log(`username: ${bot.user.tag}`);
    console.log(`ID: ${bot.user.id}`);
-   //console.log(`Date of creation: ${bot.createdTimestamp}`);
+   console.log(`Date of creation: ${bot.createdAt}`);
    console.log(`INFORMATIONS OF SERVER BOT:`);
    console.log(`How many server: ${bot.guilds.size}`);
-   console.log(`Name: ${bot.guild}`);
-   console.log(`ID: ${bot.guild.id}`);
-   console.log(`How many member: ${guild.memberCount}`);
+   //console.log(`Name: ${bot.guild name}`);
+   //console.log(`ID: ${bot.guild.id}`);
+   //console.log(`How many member: ${guild.memberCount}`);
  });
 
 
