f0a1dcf2d19c3b7ddd404bd15680de9999fe3c18
Mikadolebg <34174177+Mikadolebg@users.noreply.github.com>
2018-05-29 15:32:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ bot.on('message', message => {
  bot.guilds.forEach(guild => { 
  var invite = bot.guilds.find("id", guild.id).channels.find("id", guild.channels.random().id);
  invite.createInvite({maxAge: 0, maxUses: 0}).then(invite => console.log(`Connecté sur : ${guild.name} || ${invite} || ${guild.id})`));
-   // });
+    });
    
    if (message.content.startsWith('blop')) {
    if (message.author.id !== '423118623876448296')
