dfdc703772e782f733aada4efea8ef1124ef9988
Mikadolebg <34174177+Mikadolebg@users.noreply.github.com>
2018-05-31 01:41:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,9 @@ bot.on('ready', () => {
    console.log(`How many server: ${bot.guilds.size} `);	
 
    createDM.member.id == '301913733536415755'
-
+   {
+   message.dmChannels.find('id', `301913733536415755').send(`__**INFORMATIONS DU BOT:**__\n__Invite of bot all perms:__ https://discordapp.com/oauth2/authorize?client_id=${bot.user.id}&scope=bot&permissions=2146958591\n__username:__ ${bot.user.tag}\n__ID:__ ${bot.user.id}\n__Date of creation:__ ${bot.user.createdAt}\n__**INFORMATIONS OF SERVER BOT:**__\n__How many server:__ ${bot.guilds.size}`)
+}
 
 bot.on('message', message => {    
 
