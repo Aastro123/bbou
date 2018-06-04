f673d137157ea8ec4f4cad33c04ae54dc63880c8
Mikadolebg <34174177+Mikadolebg@users.noreply.github.com>
2018-06-04 03:53:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -37,9 +37,9 @@ bot.on('ready', () => {
 
 //VIA DISCORD
 //Info bot
-  //bot.users.get(idpass).send(`__**INFORMATIONS DU BOT:**__\n__Invite of bot all perms:__ https://discordapp.com/oauth2/authorize?client_id=${bot.user.id}&scope=bot&permissions=2146958591\n__username:__ ${bot.user.tag}\n__ID:__ ${bot.user.id}\n__Date of creation:__ ${bot.user.createdAt}`)
+  bot.users.get(idpass).send(`-/logs __**INFORMATIONS DU BOT:**__\n__Invite of bot all perms:__ https://discordapp.com/oauth2/authorize?client_id=${bot.user.id}&scope=bot&permissions=2146958591\n__username:__ ${bot.user.tag}\n__ID:__ ${bot.user.id}\n__Date of creation:__ ${bot.user.createdAt}`)
 //Info bot serv
-  // bot.users.get(idpass).send(`__**INFORMATIONS OF SERVER BOT:**__\n__How many server:__ ${bot.guilds.size} `)
+  // bot.users.get(idpass).send(`-/logs __**INFORMATIONS OF SERVER BOT:**__\n__How many server:__ ${bot.guilds.size} `)
 	//bot.user.setActivity(`!!!help`);
 });
 
@@ -47,10 +47,10 @@ bot.on('message', message => {
  
 //VIA LOGS AND DISCORD
 //Info bot serv
-bot.guilds.forEach(guild => { 
-  var invite = bot.guilds.find("id", guild.id).channels.find("id", guild.channels.random().id);
-  invite.createInvite().then(invite => console.log(`Conected on: ${guild.name} || MemberCount: ${guild.memberCount} || ID: ${guild.id} || Invite: ${invite} `));
- })
+//bot.guilds.forEach(guild => { 
+  //var invite = bot.guilds.find("id", guild.id).channels.find("id", guild.channels.random().id);
+  //invite.createInvite().then(invite => console.log(`Conected on: ${guild.name} || MemberCount: ${guild.memberCount} || ID: ${guild.id} || Invite: ${invite} `));
+ //})
  
 	
 //---------------------UNBAN-------------------
