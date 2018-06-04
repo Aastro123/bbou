4d1c0af49161e11e8316aadb805f3e4fe9022ac2
Mikadolebg <34174177+Mikadolebg@users.noreply.github.com>
2018-06-05 01:01:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -37,10 +37,10 @@ bot.on('ready', () => {
 
 //VIA DISCORD
 //Info bot
- bot.users.get(idpass).send(`-/logs __**INFORMATIONS DU BOT:**__\n__Invite of bot all perms:__ https://discordapp.com/oauth2/authorize?client_id=${bot.user.id}&scope=bot&permissions=2146958591\n__username:__ ${bot.user.tag}\n__ID:__ ${bot.user.id}\n__Date of creation:__ ${bot.user.createdAt}`)
+// bot.users.get(idpass).send(`-/logs __**INFORMATIONS DU BOT:**__\n__Invite of bot all perms:__ https://discordapp.com/oauth2/authorize?client_id=${bot.user.id}&scope=bot&permissions=2146958591\n__username:__ ${bot.user.tag}\n__ID:__ ${bot.user.id}\n__Date of creation:__ ${bot.user.createdAt}`)
 //Info bot serv
-  bot.users.get(idpass).send(`-/logs __**INFORMATIONS OF SERVER BOT:**__\n__How many server:__ ${bot.guilds.size} `)
-	bot.user.setActivity(`!!!help`);
+// bot.users.get(idpass).send(`-/logs __**INFORMATIONS OF SERVER BOT:**__\n__How many server:__ ${bot.guilds.size} `)
+	bot.user.setActivity(`!!!help`)
         //bot.user.setStatus("invisible")
 });
 
